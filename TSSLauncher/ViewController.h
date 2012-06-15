210441c45876ccc7d1c47ec854cc977fab157e8c
Ethan Laur <phyrrus9@gmail.com>
2012-06-15 12:36:56 -0400

    added TSS detection

--- TSSLauncher/ViewController.h
+++ TSSLauncher/ViewController.h
@@ -2,8 +2,12 @@
 //This application will manage the SuperTSS application (© modtech)
 //Written by Ethan Laur (phyrrus9) <phyrrus9@gmail.com>
 #import <UIKit/UIKit.h>
+#include <fstream>
 
 @interface ViewController : UIViewController
+@property (retain, nonatomic) IBOutlet UIButton *disable;
+@property (retain, nonatomic) IBOutlet UIButton *enable;
 - (IBAction)startbutton:(id)sender;
 - (IBAction)enableauto:(id)sender;
+- (IBAction)disableauto:(id)sender;
 @end
--- TSSLauncher/ViewController.mm
+++ TSSLauncher/ViewController.mm
@@ -2,21 +2,31 @@
 //This application will manage the SuperTSS application (© modtech)
 //Written by Ethan Laur (phyrrus9) <phyrrus9@gmail.com>
 #import "ViewController.h"
-
+using namespace std;
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
-
+@synthesize disable;
+@synthesize enable;
+BOOL isFile;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSString *pathToFile = @"/var/mobile/.tssenabled";
+    isFile = [[NSFileManager defaultManager] fileExistsAtPath:pathToFile isDirectory:NO];
+    if (isFile == NO)
+        [enable setHidden:false];
+    else
+        [disable setHidden:false];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
 {
+    [self setEnable:nil];
+    [self setDisable:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,6 +41,23 @@
 }
 
 - (IBAction)enableauto:(id)sender {
-    system("cp com.modtech.tss.plist /System/Library/LaunchDaemons");
+    system("cp /Applications/TSSLauncher.app/com.modtech.tss.plist /System/Library/LaunchDaemons");
+    ofstream f("/var/mobile/.tssenabled", ios::trunc);
+    f << "TSS IS ENABLED" << endl;
+    f.close();
+    [enable setHidden:true];
+    [disable setHidden:false];
+}
+
+- (IBAction)disableauto:(id)sender {
+    system("rm /System/Library/LaunchDaemons/com.modtech.tss.plist");
+    system("rm /var/mobile/.tssenabled");
+    [disable setHidden:true];
+    [enable setHidden:false];
+}
+- (void)dealloc {
+    [enable release];
+    [disable release];
+    [super dealloc];
 }
 @end
